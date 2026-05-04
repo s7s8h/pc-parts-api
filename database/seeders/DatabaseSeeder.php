a7a1293e72c4d5850d7d7fe5906ba7f0f8fa8827
Abdullah <abulastxd@gmail.com>
2026-05-05 01:13:58 +0300

    Fix database seeder imports

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,15 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-       User::firstOrCreate(
+        User::firstOrCreate(
             ['email' => 'abdullah@test.com'],
             [
                 'name' => 'Abdullah',
